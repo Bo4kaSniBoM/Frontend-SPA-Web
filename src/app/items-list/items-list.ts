794f6f3ec31a8931a7d96260f47acadb8eb8cb09
Bo4kaS.πiBoM <zhekatitovich@gmail.com>
2025-12-12 19:42:50 +0200

    пр2

--- src/app/items-list/items-list.ts
+++ src/app/items-list/items-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ProgrammingLanguage } from '../core/models/programming-language';
+import { ItemCard } from '../item-card/item-card';
 
 @Component({
   selector: 'app-items-list',
-  imports: [],
+  imports: [ItemCard],
   templateUrl: './items-list.html',
   styleUrl: './items-list.css',
 })

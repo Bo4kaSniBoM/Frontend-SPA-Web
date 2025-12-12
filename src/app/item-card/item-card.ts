a50e6bf0764455eadfbba4c4babdcae27692f335
Bo4kaS.πiBoM <zhekatitovich@gmail.com>
2025-12-12 20:58:54 +0200

    пр3

--- src/app/item-card/item-card.ts
+++ src/app/item-card/item-card.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import {ProgrammingLanguage} from '../core/models/programming-language';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-item-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './item-card.html',
   styleUrl: './item-card.css',
 })
 export class ItemCard {
+  @Input() language!: ProgrammingLanguage;
 
+  getRatingColor(rating: number): string {
+    if (rating >= 9) return '#2ecc71';
+    if (rating >= 6) return '#f1c40f';
+    return '#95a5a6';
+  }
 }
